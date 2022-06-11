97a39daef6957dbc9eb9fbc91cc3e797f10b8c86
Asl161997 <karlitaherrera365@gmail.com>
2022-06-11 17:59:09 -0500

    crud producto v01

---
+++ Inventario_Rest/controllers/productos-controller.js
@@ -0,0 +1,80 @@
+const req = require('express/lib/request')
+const { db } = require('../config/connection')
+
+const getProductos = async (req,res)=>{
+    const response = await db.any(`select pro_id,pro_nombre,pro_descripcion, 
+    pro_iva,pro_costo,pro_pvp,pro_stock,pro_imagen,pro_estado from producto;`)
+    res.json(response)
+}
+
+const getProductosById=async(req,res)=>{
+    const pro_id=req.params.pro_id
+    const response = await db.any(`select pro_id,pro_nombre,pro_descripcion, 
+    pro_iva,pro_costo,pro_pvp,pro_stock,pro_imagen,pro_estado from producto where pro_id=$1::int;`,[pro_id])
+    res.json(response)
+}
+
+
+const getProductosByName=async(req,res)=>{
+    const pro_nombre=req.params.pro_nombre
+    const response = await db.any(`select pro_id,pro_nombre,pro_descripcion, 
+    pro_iva,pro_costo,pro_pvp,pro_stock,pro_imagen,pro_estado from producto where pro_nombre=$1`,[pro_nombre])
+    res.json(response)
+}
+
+const postCreateProducto = async(req, res) =>{
+    const {pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado} = req.query
+    const response = await db.any(`INSERT INTO public.pizza(pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado))
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);`,[pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado])
+    res.json(
+        {
+            message: "Producto creado con éxito",
+            body:{
+                producto: {pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado}
+            }
+        }
+    )
+}
+
+// const createProducto=async(req,res)=>{
+//     const{pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado}=req.query
+//     const response = await db.one(`INSERT INTO producto(
+//         pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado))
+//         VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9 ) returning*;`,[pro_nombre, pro_descripcion, cat_id, pro_iva, pro_costo, pro_pvp, pro_stock, pro_imagen, pro_estado])
+//         res.json(response)
+// }
+
+const putUpdateProducto = async(req, res) =>{
+    const {pro_id,pro_nombre,pro_descripcion,cat_id,
+        pro_iva,pro_costo,pro_pvp,pro_stock,pro_imagen,pro_estado} = req.query
+    const response = await db.any('UPDATE producto SET pro_nombre=$2, pro_descripcion=$3, cat_id=$4, pro_iva=$5, pro_costo=$6, pro_pvp=$7, pro_stock=$8, pro_imagen=$9, pro_estado=$10 WHERE pro_id=$1',[pro_id,pro_nombre,pro_descripcion,cat_id,
+        pro_iva,pro_costo,pro_pvp,pro_stock,pro_imagen,pro_estado])
+    res.json(
+        {
+            message: "Producto actualizado con éxito",
+            body:{
+                producto: {pro_id,pro_nombre,pro_descripcion,cat_id,
+                    pro_iva,pro_costo,pro_pvp,pro_stock,pro_imagen,pro_estado}
+            }
+        }
+    )
+}
+
+const deleteProducto = async(req, res) =>{
+    const {pro_id} = req.query
+    const response = await db.any('DELETE FROM producto WHERE pro_id=$1',[pro_id])
+    res.json(
+        {
+            message: "Producto eliminado con éxito",
+            body:{
+                producto: {pro_id}
+            }
+        }
+    )
+}
+
+module.exports = {
+    getProductos,getProductosById,
+    getProductosByName,postCreateProducto,
+    putUpdateProducto,deleteProducto
+}
--- Inventario_Rest/routes/index.js
+++ Inventario_Rest/routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
 
-const{}=require('../controllers/inventario-controller')
+const { getProductos, getProductosById, postCreateProducto,putUpdateProducto, deleteProducto, getProductosByName} = require("../controllers/productos-controller");
 const router= Router()
 
-
+router.get('/productos',getProductos)
+router.get('/productos/pro_id/:pro_id',getProductosById)
+router.get('/productos/pro_nombre/:pro_nombre',getProductosByName)
+router.post('/productos/',postCreateProducto)
+router.put("/productos",putUpdateProducto)
+router.delete('/productos',deleteProducto)
 
 module.exports=router
