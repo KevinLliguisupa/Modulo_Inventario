ea73a09e03a08bb7edf2803de015195f67d5801e
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2022-06-25 15:23:11 -0500

    Uso de cors

--- Inventario_Rest/src/index.js
+++ Inventario_Rest/src/index.js
@@ -1,14 +1,16 @@
-// const { db } = require("../config/connection");
-// db.any('select * from "producto"').then(res=>{console.table(res)})
-
 const express = require('express')
 const app= express()
 //para reconocer html
 const bodeParser = require('body-parser')
+//Para conexion con front 
+const cors = require('cors')
 
 //midlewears
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+//Autentificacion cors
+app.use(cors({ origin: true, credentials: true  }));
 
 app.use(require('../routes/index'))
 //execution server web
